c02ee9c28747a96fe50674c3dc623eea45329bb2
yurubahmed <yurubiftin2002@gmail.com>
2023-03-02 21:51:22 -0500

    seeds.rb updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,48 +2,18 @@ puts ":seedling: Seeding spices..."
 # Seed your database here
 # db/seeds.rb
 # Seed genres
-action_genre = Genre.create(name: "Action")
-comedy_genre = Genre.create(name: "Comedy")
-drama_genre = Genre.create(name: "Drama")
-
+Genre.create(name: "Action")
+Genre.create(name: "Comedy")
+Genre.create(name: "Drama")
 # Seed users
-john = User.create(name: "John Doe")
-jane = User.create(name: "Jane Smith")
-
+User.create(name: "John Doe")
+User.create(name: "Jane Smith")
 # Seed movies
-die_hard = Movie.create(
-  title: "Die Hard",
-  genre: action_genre
-)
-die_hard.image.attach(
-  io: File.open('path/to/die-hard.jpg'),
-  filename: 'die-hard.jpg',
-  content_type: 'image/jpeg'
-)
-
-bridesmaids = Movie.create(
-  title: "Bridesmaids",
-  genre: comedy_genre
-)
-bridesmaids.image.attach(
-  io: File.open('path/to/bridesmaids.jpg'),
-  filename: 'bridesmaids.jpg',
-  content_type: 'image/jpeg'
-)
-
-shawshank_redemption = Movie.create(
-  title: "The Shawshank Redemption",
-  genre: drama_genre
-)
-shawshank_redemption.image.attach(
-  io: File.open('path/to/shawshank-redemption.jpg'),
-  filename: 'shawshank-redemption.jpg',
-  content_type: 'image/jpeg'
-)
-
+Movie.create(title: "Die Hard", genre: Genre.find_by(name: "Action"), image_url: "https://example.com/die_hard.jpg")
+Movie.create(title: "Bridesmaids", genre: Genre.find_by(name: "Comedy"), image_url: "https://example.com/bridesmaids.jpg")
+Movie.create(title: "The Shawshank Redemption", genre: Genre.find_by(name: "Drama"), image_url: "https://example.com/shawshank_redemption.jpg")
 # Seed ratings
-Rating.create(movie: die_hard, user: john, score: 4)
-Rating.create(movie: bridesmaids, user: jane, score: 5)
-Rating.create(movie: shawshank_redemption, user: john, score: 5)
-
+Rating.create(movie: Movie.find_by(title: "Die Hard"), user: User.find_by(name: "John Doe"), score: 4)
+Rating.create(movie: Movie.find_by(title: "Bridesmaids"), user: User.find_by(name: "Jane Smith"), score: 5)
+Rating.create(movie: Movie.find_by(title: "The Shawshank Redemption"), user: User.find_by(name: "John Doe"), score: 5)
 puts ":white_check_mark: Done seeding!"
