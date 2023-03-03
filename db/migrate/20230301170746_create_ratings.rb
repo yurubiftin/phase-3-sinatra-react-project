3252fc8411b1f32c50b26c0002164821b09fd602
yurubahmed <yurubiftin2002@gmail.com>
2023-03-02 21:59:25 -0500

    schema updated successfully

--- db/migrate/20230301170746_create_ratings.rb
+++ db/migrate/20230301170746_create_ratings.rb
@@ -1,5 +1,6 @@
 class CreateRatings < ActiveRecord::Migration[6.1]
   def change
+    
     create_table :ratings do |t|
       t.references :movie, index: true
       t.references :user, index: true
